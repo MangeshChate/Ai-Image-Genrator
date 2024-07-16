fb896d40914eeca4ae9eaecaf7cc2dd97b20327a
mangesh <mbchate08@gmail.com>
2024-07-17 05:28:08 +0530

    commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,20 @@ const Navbar = () => {
     <div>
       <div className="container pt-5 pb-5 flex justify-between items-center">
         <Link href={"/"} className='flex items-center gap-3'>
-         <Image src={logo} alt="logo.png" width="50" height="50"/>
+          <Image src={logo} alt="logo.png" width="50" height="50" />
           <span className='font-bold text-2xl'>PurpleAi</span>
         </Link>
         <div className='hidden sm:hidden md:block lg:block'>
           <ul className='flex gap-10 '>
-            <Link href="/">Home</Link>
-            <li>Explore</li>
-            <li>Contact</li>
+            <li key="home"><Link href="/">Home</Link></li>
+            <li key="explore">Explore</li>
+            <li key="contact">Contact</li>
           </ul>
         </div>
         <div className='flex gap-4'>
           <Button className='hidden sm:hidden md:hidden lg:block'>Login</Button>
           <Link href={"/dashboard"}>
-          <Button className='bg-transparent border-2 rounded-lg border-purple-800'>Lets Genrate</Button>
+            <Button className='bg-transparent border-2 rounded-lg border-purple-800'>Lets Genrate</Button>
           </Link>
         </div>
       </div>
