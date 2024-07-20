c5a05af905671fd8814e4b92a17e678896e9e86c
mangesh <mbchate08@gmail.com>
2024-07-21 04:40:57 +0530

    commit

--- src/components/Testamonial.tsx
+++ src/components/Testamonial.tsx
@@ -34,7 +34,7 @@ const testimonials = [
 
 const Testamonial = () => {
   return (
-    <section className="py-20 sm:py-32 grid-bg text-white">
+    <section className="py-20 sm:py-32 grid-bg text-white mt-[100px]">
     <div className="container mx-auto px-4 sm:px-6 lg:px-8">
       <div className="text-center mb-16">
         <h2 className="text-4xl sm:text-5xl lg:text-6xl font-bold tracking-tight mb-4">
