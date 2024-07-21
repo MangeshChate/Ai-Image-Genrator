2e7a457e6334e51505edefd539cf66e95772cfa6
mangesh <mbchate08@gmail.com>
2024-07-21 09:10:25 +0530

    commit

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL =  'http://localhost:3000/api';
+const API_BASE_URL =  'https://ai-image-genrator-seven.vercel.app/api' || 'http://localhost:3000/api';
 // "https://ai-image-genrator-seven.vercel.app/api"
 
 // src/services/apiService.ts
