7faff3b4507e3edcaca13304189ee149bb114f8e
mangesh <mbchate08@gmail.com>
2024-07-21 11:25:17 +0530

    commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,17 @@
+// app/not-found.tsx
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const NotFound: React.FC = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/');
+  }, [router]);
+
+  return null;
+};
+
+export default NotFound;
